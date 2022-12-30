faf9ef36ffded5d2d92e9d76402545c3c0a96e78
Gaurav Pathak <108235788+gauravpathak346@users.noreply.github.com>
2022-12-31 02:02:08 +0530

    Add files via upload

---
+++ p.py
@@ -0,0 +1,25 @@
+# import math
+# def squareRoot(n, l) :
+# # Assuming the sqrt of n as n only
+#     x = n
+# # To count the number of iterations
+#     count = 0
+#     while (1) :
+#         count += 1
+# # Calculate more closed x
+#         root = 0.5 * (x + (n / x))
+# # Check for closeness
+#         if (abs(root - x) < l) :
+#          break
+# # Update root
+#         x = root
+#     return root
+# # Driver code
+# if __name__ == "__main__" :
+#     n = 327
+#     l = 0.00001
+#     print(squareRoot(n,1))
+a=3
+b=4
+result = a**b
+print(result)
